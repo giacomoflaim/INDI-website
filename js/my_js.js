03068ab4e8d52f37c3b73e6529ba6e2e8f0504a8
Giacomo <giacomo.flaim.91@gmail.com>
2020-03-16 15:42:27 +0100

    old-1

---
+++ js/my_js.js
@@ -0,0 +1,7 @@
+/*$(window).scroll(function() {
+  if ($(this).scrollTop() > 740) {
+    $(".kit-button").removeClass("bottom-right").addClass("top-right");
+  } else {
+    $(".kit-button").removeClass("top-right").addClass("bottom-right");
+  }
+});*/
